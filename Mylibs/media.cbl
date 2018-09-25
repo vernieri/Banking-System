16d44e37b910abde9948f71afa37daf180fadc4c
Vernieri <31966345+vernieri@users.noreply.github.com>
2018-09-25 14:24:20 -0300

    Update media.cbl

--- Mylibs/media.cbl
+++ Mylibs/media.cbl
@@ -45,3 +45,15 @@
            02 ANO               PIC 9(02) VALUE ZEROS.
            02 MES               PIC 9(02) VALUE ZEROS.
            02 DIA               PIC 9(02) VALUE ZEROS.
+      
+       SCREEN SECTION.
+       01 TELA01.
+           02 LINE 02 COLUMN 05 PIC 9(02)/ USING DIA.
+           02 LINE 02 COLUMN 08 PIC 9(02)/ USING MES.
+           02 LINE 02 COLUMN 11 PIC 9(02)  USING ANO.
+           02 LINE 02 COLUMN 28 VALUE
+                "Cálcula da Média das Notas".
+           02 LINE 08 COLUMN 15 VALUE "Nota 1:".
+           02 LINE 10 COLUMN 15 VALUE "Nota 2:".
+           02 LINE 12 COLUMN 15 VALUE "Nota 3:".
+           02 LINE 14 COLUMN 15 VALUE "Média :".      
