5552984e9944b6d6ef28b047c216209851827527
Vernieri <jvmendes.ti@gmail.com>
2018-10-27 02:27:35 -0300

    Update Livros.cbl

--- Mylibs/Livros.cbl
+++ Mylibs/Livros.cbl
@@ -108,3 +108,254 @@
            02 line 14 col 19 value "Doador: ".
            02 line 15 col 19 value "Preco: ".
            02 line 16 col 19 value "Procedencia: ".
+
+             Procedure Division.
+       Inicio.
+           Perform Abre-arq.
+           Move function current-date to data-sis.
+           Perform Processo until op = 6.
+           Perform Finaliza.
+           stop run.
+
+       Abre-arq.
+           Open i-o BBLIVROS.
+           If arqst not = "00"
+               Display "erro de abertura" at 2110
+               Close BBLIVROS
+               Open output BBLIVROS.
+
+       Processo.
+           Perform mostra-tela-menu.
+           move spaces to op-continua.
+           Accept op at 1845 with prompt auto.
+           Evaluate op
+           when 1
+               perform inclusao until op-continua = "n" or "N"
+           when 2
+               perform alteracao until op-continua = "n" or "N"
+           when 3
+               perform exclusao until op-continua = "n" or "N"
+           when 4
+               perform consulta-tombo until op-continua = "n" or "N"
+           when 5
+               perform consulta-titulo until op-continua = "n" or "N".
+
+       Inclusao.
+           Perform mostra-tela-cadastro.
+           Display "I N C L U S A O" at 0630 with highlight.
+           Perform entra-tombo until wflag = 1.
+           Perform recebe-dados.
+           Perform gravar-livro.
+           Perform continua.
+
+       Mostra-tela-menu.
+           Display tela-menu.
+           Display mes-t(mes) at 0219.
+           move zeros to op.
+
+       Mostra-tela-cadastro.
+           Display tela-cadastro.
+           Display mes-t(mes) at 0219.
+           move zeros to wflag w-tombo w-preco editadas
+                         w-emprestado w-ano-livro.
+           move spaces to op-continua salva.
+           move spaces to w-titulo w-autor w-editora.
+           move spaces to w-doacao w-doador w-procedencia.
+
+       Recebe-dados.
+           perform entra-titulo   until wflag = 0.
+           perform entra-autor    until wflag = 1.
+           perform entra-editora  until wflag = 0.
+           perform entra-ano      until wflag = 1.
+           perform entra-doacao   until wflag = 0.
+           if w-doacao = "S"
+               perform entra-doador        until wflag = 1
+           else
+                perform entra-preco         until wflag = 1
+                perform entra-procedencia   until wflag = 0.
+
+       Entra-Tombo.
+           move 1 to wflag.
+           move zeros to w-tombo-e.
+           accept w-tombo-e at 0826 with prompt auto.
+           move w-tombo-e to w-tombo tombo.
+           if w-tombo = 999999 then
+                Close BBLIVROS
+                Exit program.
+           if op = 1
+                read BBLIVROS key is tombo not invalid key perform
+                ja-cadastrado
+           else
+                read BBLIVROS key is tombo invalid key perform
+                nao-cadastrado.
+
+       Ja-cadastrado.
+           display "Codigo ja cadastrado" at 2321
+           set wflag to 0.
+
+       Nao-cadastrado.
+           display "Codigo nao cadastrado" at 2321
+           set wflag to 0.
+
+       Entra-titulo.
+           accept w-titulo at 0927 with prompt auto.
+           if w-titulo = spaces then
+                display "Digite o Titulo do Livro." at 2321
+           else
+                display espaco at 2321
+                move 0 to wflag.
+
+       Entra-autor.
+           accept w-autor at 1027 with prompt auto.
+           if w-autor = spaces then
+                display "Digite o Autor do Livro." at 2321
+           else
+                display espaco at 2321
+                move 1 to wflag.
+
+       Entra-Editora.
+           accept w-editora at 1128 with prompt auto.
+           if w-editora = spaces then
+                display "Digite a Editora do Livro." at 2321
+           else
+                display espaco at 2321
+                move 0 to wflag.
+
+       Entra-ano.
+           accept w-ano-livro at 1224 with prompt auto.
+           if w-ano-livro = zeros then
+                display "Digite o Ano do Livro." at 2321
+           else
+                If (w-ano-livro > 2000) and (w-ano-livro <= ano)
+                   display espaco at 2321
+                   move 1 to wflag.
+
+       Entra-Doacao.
+           accept w-doacao at 1327 with prompt auto.
+           if w-doacao = "S" or "N" then
+                display espaco at 2321
+                move 0 to wflag
+           else
+                display "Digite <S> SIM ou <N> NAO" at 2321.
+
+       Entra-doador.
+           accept w-doador at 1427 with prompt auto.
+           if w-doador = spaces then
+                display "Digite o Doador do Livro." at 2321
+           else
+                display espaco at 2321
+                move 1 to wflag.
+
+       Entra-preco.
+           accept w-preco-e at 1526 with prompt auto.
+           move w-preco-e to w-preco.
+           if w-preco = zeros then
+                display "Digite o Preco do Livro." at 2321
+           else
+                display espaco at 2321
+                move 1 to wflag.
+
+       Entra-procedencia.
+           accept w-procedencia at 1632 with prompt auto.
+           if w-procedencia = spaces then
+                display "Digite a Procedencia do Livro." at 2321
+           else
+                display espaco at 2321
+                move 0 to wflag.
+
+       Gravar-livro.
+           Display "Gravar Registro? <S/N> [ ]" at 2321.
+           Accept salva at 2345 with prompt auto.
+           If salva = "S" or "s" then
+               Move w-tombo       to tombo
+               Move w-titulo      to titulo
+               Move w-autor       to autor
+               Move w-editora     to editora
+               Move w-ano-livro   to ano-livro
+               Move w-doacao      to doacao
+               Move w-doador      to doador
+               Move w-preco       to preco
+               Move w-procedencia to procedencia
+               Move w-emprestado  to emprestado
+               If op = 1
+                   Write reg-livro invalid key perform estuda-erro
+               else
+                   Rewrite reg-livro invalid key perform estuda-erro.
+
+       Continua.
+           display espaco at 2321.
+           display "Continua <S/N> [ ]" at 2321.
+           accept op-continua at 2337 with prompt auto.
+           if op-continua = "S" or "s" then
+                display espaco at 2315.
+
+       Estuda-erro.
+           display "Erro de atualização." at 2321.
+           display arqst at 2221.
+           stop " ".
+
+       Alteracao.
+           perform mostra-tela-cadastro.
+           Display "A L T E R A C A O" at 0630 with highlight.
+           Display "Empresatado: " at 1719.
+           Perform entra-tombo until wflag = 1.
+           Perform mostra-dados.
+           Perform recebe-dados.
+           accept w-emprestado at 1732 with prompt auto.
+           Perform gravar-livro.
+           Perform continua.
+
+       Mostra-dados.
+           Move reg-livro to w-reg-livro.
+           Move w-preco to w-preco-e
+           Display w-titulo at 0927.
+           Display w-autor at 1027.
+           Display w-editora at 1128.
+           Display w-ano-livro at 1224.
+           Display w-doacao at 1327.
+           Display w-doador at 1427.
+           Display w-preco-e at 1526.
+           Display w-procedencia at 1632.
+           Display w-emprestado at 1732.
+
+       Exclusao.
+           perform mostra-tela-cadastro.
+           Display "E X C L U S A O" at 0630 with highlight.
+           Perform entra-tombo until wflag = 1.
+           Perform mostra-dados.
+           Perform excluir-livro.
+           Perform continua.
+
+       Excluir-livro.
+           display espaco at 2315.
+           display "Excluir? <S/N> [ ]" at 2321.
+           accept salva at 2337 with prompt auto.
+           if salva = "S" or "s" then
+                Delete BBLIVROS invalid key perform
+                estuda-erro
+                display espaco at 2315.
+
+       Consulta-tombo.
+           perform mostra-tela-cadastro.
+           Display "C O N S U L T A   T O M B O" at 0630 with highlight.
+           Perform entra-tombo until wflag = 1.
+           Perform mostra-dados.
+           Perform continua.
+
+       Consulta-titulo.
+           perform mostra-tela-cadastro.
+           Display
+           "C O N S U L T A   T I T U L O" at 0630 with highlight.
+           Perform entra-titulo.
+           move w-titulo to titulo.
+           read BBLIVROS key is titulo
+               invalid key
+                   display "Livro nao encontrado!" at 1835
+               not invalid key
+                   Perform mostra-dados
+                   Display tombo at 0826.
+           Perform continua.
+
+       Finaliza.
+           close BBLIVROS.
+           Display "F I M   D O   P R O G R A M A" at 1530.
